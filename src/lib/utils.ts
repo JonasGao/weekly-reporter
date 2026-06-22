86408937b9829690652965480e410591ff838978
Jonas <huoju@madmod.cn>
2026-06-22 13:47:13 +0800

    chore: setup shadcn/ui and add components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,6 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-import { format, startOfWeek, endOfWeek } from 'date-fns'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function formatDate(date: Date): string {
-  return format(date, 'yyyy-MM-dd')
-}
-
-export function getWeekRange(date: Date): { start: Date; end: Date } {
-  const start = startOfWeek(date, { weekStartsOn: 1 })
-  const end = endOfWeek(date, { weekStartsOn: 1 })
-  return { start, end }
 }
